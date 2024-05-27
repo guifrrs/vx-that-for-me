18c9eb6ba65b47e7f7c302323058cf94aa44ce32
Guilherme Silva <gui.silva@tutanota.com>
2024-05-27 20:14:39 -0300

    feat: delete original message and make bot kawai

--- handlers.go
+++ handlers.go
@@ -5,6 +5,10 @@ import "github.com/yanzay/tbot/v2"
 func (app *Application) MessageHandler(msg *tbot.Message) {
 	tweetLink := "https://fixvx.com/" + msg.Text[20:]
 
-	app.client.SendMessage(msg.Chat.ID, "Here, I fixed that for you")
-	app.client.SendMessage(msg.Chat.ID, tweetLink)
+	app.client.SendMessage(msg.Chat.ID, "Here, I fixed that for you :3")
+	_, err := app.client.SendMessage(msg.Chat.ID, tweetLink)
+
+	if err == nil {
+		app.client.DeleteMessage(msg.Chat.ID, msg.MessageID)
+	}
 }
