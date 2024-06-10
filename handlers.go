0f8ad27bd2a6e98c18b4daf72fefe52124ee5bf3
Guilherme Silva <gui.silva@tutanota.com>
2024-06-10 19:50:23 -0300

    chore: add debug log

--- handlers.go
+++ handlers.go
@@ -3,12 +3,17 @@ package main
 import (
 	"fmt"
 
+	"log"
+
 	"github.com/yanzay/tbot/v2"
 )
 
 func (app *Application) MessageHandler(msg *tbot.Message) {
 	tweetLink := "https://fixvx.com/" + msg.Text[20:]
 	originalSenderMsg := fmt.Sprintf("Hey @%s, I fixed that for you :3", msg.Chat.Username)
+
+	log.Println("Sender: ", msg.Chat.Username)
+
 	app.client.SendMessage(msg.Chat.ID, originalSenderMsg)
 	_, err := app.client.SendMessage(msg.Chat.ID, tweetLink)
 
