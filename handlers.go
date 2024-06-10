dcf2c5dd7a743ff1b4ee525a579c467f6bca5c1c
Guilherme Silva <gui.silva@tutanota.com>
2024-06-10 18:07:14 -0300

    feat: add original sender with fixed msg

--- handlers.go
+++ handlers.go
@@ -1,11 +1,15 @@
 package main
 
-import "github.com/yanzay/tbot/v2"
+import (
+	"fmt"
+
+	"github.com/yanzay/tbot/v2"
+)
 
 func (app *Application) MessageHandler(msg *tbot.Message) {
 	tweetLink := "https://fixvx.com/" + msg.Text[20:]
-
-	app.client.SendMessage(msg.Chat.ID, "Here, I fixed that for you :3")
+	originalSenderMsg := fmt.Sprintf("Hey @%s, I fixed that for you :3", msg.From.Username)
+	app.client.SendMessage(msg.Chat.ID, originalSenderMsg)
 	_, err := app.client.SendMessage(msg.Chat.ID, tweetLink)
 
 	if err == nil {
