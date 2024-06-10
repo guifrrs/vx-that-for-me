6856929e42539423cd35f3919087deeaadb0ff6f
Guilherme Silva <gui.silva@tutanota.com>
2024-06-10 18:56:53 -0300

    fix: use chat username instead of user.username since not all users have it defined

--- handlers.go
+++ handlers.go
@@ -8,7 +8,7 @@ import (
 
 func (app *Application) MessageHandler(msg *tbot.Message) {
 	tweetLink := "https://fixvx.com/" + msg.Text[20:]
-	originalSenderMsg := fmt.Sprintf("Hey @%s, I fixed that for you :3", msg.From.Username)
+	originalSenderMsg := fmt.Sprintf("Hey @%s, I fixed that for you :3", msg.Chat.Username)
 	app.client.SendMessage(msg.Chat.ID, originalSenderMsg)
 	_, err := app.client.SendMessage(msg.Chat.ID, tweetLink)
 
