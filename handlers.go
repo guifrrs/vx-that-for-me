dc145879485f1aecfdf0f04f8ee5626d53452580
Guilherme Silva <gui.silva@tutanota.com>
2025-02-28 15:39:44 -0300

    fix: replace only the domain, keep current link as fallback, add tests

--- handlers.go
+++ handlers.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"regexp"
 
 	"github.com/yanzay/tbot/v2"
 )
@@ -14,15 +15,23 @@ func getUsername(msg *tbot.Message) string {
 	return msg.From.FirstName
 }
 
+func replaceLink(msg *tbot.Message) string {
+	domainRegex := regexp.MustCompile(`https://(.*?)/`)
+	domain := domainRegex.FindString(msg.Text)
+	if domain != "" {
+		return "https://fixvx.com/" + msg.Text[len(domain):]
+	}
+
+	return msg.Text
+}
+
 func MessageHandler(msg *tbot.Message) {
 	logMsg := "Received message from @" + getUsername(msg) + ": " + msg.Text
 	log.Println(logMsg)
 
-	tweetLink := "https://fixvx.com/" + msg.Text[20:]
 	originalSenderMsg := "Hey @" + getUsername(msg) + ", I fixed that for you :3"
-
 	app.client.SendMessage(msg.Chat.ID, originalSenderMsg)
-	_, err := app.client.SendMessage(msg.Chat.ID, tweetLink)
+	_, err := app.client.SendMessage(msg.Chat.ID, replaceLink(msg))
 	if err != nil {
 		log.Println("Error sending message:", err)
 	}
---
+++ handlers_test.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/yanzay/tbot/v2"
+)
+
+func TestReplaceLink(t *testing.T) {
+	testCases := []struct {
+		name     string
+		msg      *tbot.Message
+		expected string
+	}{
+		{
+			name: "Normal link",
+			msg: &tbot.Message{
+				Text: "https://x.com/whotfisjovana/status/1894867871412691219",
+			},
+			expected: "https://fixvx.com/whotfisjovana/status/1894867871412691219",
+		},
+		{
+			name: "Broken link",
+			msg: &tbot.Message{
+				Text: "https://x.com/theo/status/1895247223577026832",
+			},
+			expected: "https://fixvx.com/theo/status/1895247223577026832",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			actual := replaceLink(tc.msg)
+			if actual != tc.expected {
+				t.Errorf("Expected %s, got %s", tc.expected, actual)
+			}
+		})
+	}
+}
